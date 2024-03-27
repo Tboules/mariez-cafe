73449c3e9a5e77a62a54c6cc57e42c304163c7ea
Tboules <tboules@gmail.com>
2024-03-27 15:18:54 -0700

    env d ts file

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,22 @@
-/// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly FIREBASE_PRIVATE_KEY_ID: string;
+  readonly FIREBASE_PRIVATE_KEY: string;
+  readonly FIREBASE_PROJECT_ID: string;
+  readonly FIREBASE_CLIENT_EMAIL: string;
+  readonly FIREBASE_CLIENT_ID: string;
+  readonly FIREBASE_AUTH_URI: string;
+  readonly FIREBASE_TOKEN_URI: string;
+  readonly FIREBASE_AUTH_CERT_URL: string;
+  readonly FIREBASE_CLIENT_CERT_URL: string;
+
+  readonly PUBLIC_FB_API_KEY: string;
+  readonly PUBLIC_FB_AUTH_DOMAIN: string;
+  readonly PUBLIC_FB_PROJECT_ID: string;
+  readonly PUBLIC_FB_STORAGE_BUCKET: string;
+  readonly PUBLIC_FB_MESSAGING_SENDER_ID: string;
+  readonly PUBLIC_FB_APP_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
